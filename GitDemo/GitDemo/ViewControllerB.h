63a54a6a7152356243beb4b33233c8fbbd824f11
2524742620@qq.com <qianfeng@2016.local>
2016-02-26 19:36:40 +0800

    B commit changed

---
+++ GitDemo/GitDemo/ViewControllerB.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.h
+//  GitDemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 朱晓芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerB : UIViewController
+
+@end
