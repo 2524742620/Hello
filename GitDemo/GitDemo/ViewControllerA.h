071484db5092b56da4bf9e60918dbf95876506b3
2524742620@qq.com <qianfeng@2016.local>
2016-02-26 19:30:05 +0800

    A commit changed

---
+++ GitDemo/GitDemo/ViewControllerA.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerA.h
+//  GitDemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 朱晓芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerA : UIViewController
+
+@end
---
+++ GitDemo/GitDemo/ViewControllerA.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerA.m
+//  GitDemo
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 朱晓芳. All rights reserved.
+//
+
+#import "ViewControllerA.h"
+
+@interface ViewControllerA ()
+
+@end
+
+@implementation ViewControllerA
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
